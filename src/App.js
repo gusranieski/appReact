b0d23662d640c7d94ed09ce21fa251e1a27c37a7
gustavo.rañieski <gustavoranieski@hotmail.com>
2022-09-16 18:52:56 -0300

    creacion de mi app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          holaaa
         </p>
         <a
           className="App-link"
