da870f62b0100cc170ee33a0c6139bf127c267e6
gustavo.rañieski <gustavoranieski@hotmail.com>
2022-11-02 12:06:49 -0300

    config .env

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfs7Ce74zLPp5SX3S1R824uZ0sAxCEILQ",
-  authDomain: "appranieskibackend.firebaseapp.com",
-  projectId: "appranieskibackend",
-  storageBucket: "appranieskibackend.appspot.com",
-  messagingSenderId: "83102202327",
-  appId: "1:83102202327:web:5b5e6805fc6209975ef392"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
